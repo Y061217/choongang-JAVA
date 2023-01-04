936389d3039384a3b644a5e36ea935b4d96e1609
user <user@DESKTOP-G8A7MGR>
2023-01-04 11:20:09 +0900

    주민번호 코드

--- Java230102/src/Java230102/Humans.java
+++ Java230102/src/Java230102/Humans.java
@@ -7,7 +7,11 @@ public class Humans {
 	// 생성자 : 인스턴스를 발생시키는 코드
 	// 메소드 : 펑션, 기능, 동작, 동사형
 	Animal ani;
-
+	
+	public int anyoneOpen; // 서로 다른 패키지여도 사용 가능
+	protected int onlyInheritance; // 같은 패키지, 상속 관계에 사용 가능
+	private String Own; // 다른 클래스에서 호출할 수 없음
+	
 	public void show() {
 		ani = new Animal();
 		// ani.name; 불러올 수 없음
@@ -20,7 +24,7 @@ public class Humans {
 //private의 값을 저장,호출할 때 필요한 메소드 getter, setter
 class Animal {
 	private String name; // 다른 클래스에서 호출할 수 없음
-
+	protected int count;
 	public String sleep() {
 		return "영영 모르겠쥐";
 	}
