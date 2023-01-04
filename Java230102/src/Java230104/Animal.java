52296dca2be6efdb5e099e63295a2970051a3a67
user <user@DESKTOP-G8A7MGR>
2023-01-04 11:25:32 +0900

    생성자 관련 복습 내용

---
+++ Java230102/src/Java230104/Animal.java
@@ -0,0 +1,16 @@
+package Java230104;
+
+public class Animal {
+	public Animal(String name , String sex) {
+		// TODO Auto-generated constructor stub
+		System.out.println("새로운 친구가 찾아왔습니다.");
+		System.out.printf("이름은 %s이고 성별은 %s입니다.\n",name,sex);
+	}
+	public Animal() {
+		System.out.println("이름이 없는 친구가 찾아왔습니다.");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
