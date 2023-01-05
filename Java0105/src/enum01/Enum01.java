8b082a181ce0781c317783c2c6ae3909360823f3
user <user@DESKTOP-G8A7MGR>
2023-01-05 11:04:08 +0900

    var 데이터 타입

--- Java0105/src/enum01/Enum01.java
+++ Java0105/src/enum01/Enum01.java
@@ -13,6 +13,14 @@ public class Enum01 {
 	public static void main(String[] args) {
 		Week week = Week.FRI; 
 		System.out.println(week);
+		
+		var number = 10;
+		//값으로 데이터타입을 추론합니다. 지역변수로만 가능
+		//꼭 초기화 후 사용, null 초기화 안됨, 배열은 사용 불가
+		System.out.println(number);
+		
+		var name = "ㅇㅇ";
+		String str = name;
 	}
 
 }
