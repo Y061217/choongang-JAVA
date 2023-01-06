a440fb810f4a78e1e917ade96f32e37e3f489119
user <user@DESKTOP-G8A7MGR>
2023-01-06 14:16:47 +0900

    랜덤한 숫자 만들기

---
+++ Java0106/src/coll/KeyMaker.java
@@ -0,0 +1,11 @@
+package coll;
+
+public class KeyMaker {
+public static void main(String[] args) {
+	int r = (int)(Math.random()*10);
+	
+	
+	char ra = (char)((Math.random()*26)+65);
+	System.out.println(ra);
+}
+}
