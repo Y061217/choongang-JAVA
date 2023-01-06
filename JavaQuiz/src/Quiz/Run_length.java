9031704593040d4dfb150f6d85855a2c71313bce
user <user@DESKTOP-G8A7MGR>
2023-01-06 17:49:47 +0900

    숙제 : 문자열 압축하기

---
+++ JavaQuiz/src/Quiz/Run_length.java
@@ -0,0 +1,22 @@
+package Quiz;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Run_length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//aaabbcccccca
+		//a3b2c6a1
+		
+		String given = "aaabbcccccca";
+		char[] givenc = given.toCharArray();
+
+		
+
+	}
+
+}
