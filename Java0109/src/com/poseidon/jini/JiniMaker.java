2f130334a0f7e4e7051f1b342f89e36de65eed1a
user <user@DESKTOP-G8A7MGR>
2023-01-09 14:06:54 +0900

    jar 데이터 연동

---
+++ Java0109/src/com/poseidon/jini/JiniMaker.java
@@ -0,0 +1,15 @@
+package com.poseidon.jini;
+
+import java.util.Scanner;
+
+public class JiniMaker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		JINI jini = new JINI();
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
---
+++ Java0109/src/jan09/Jar01.java
@@ -0,0 +1,16 @@
+package jan09;
+//데이터 연동 jar
+public class Jar01 {
+/*
+ * jar(Java Archive) .class
+ * 
+ * war(Web) 웹 배포 형태
+ * 
+ * ear(Enterprise)
+ * 
+ */
+	
+	public static void main(String[] args) {
+		System.out.println("묶어보겠습니다.");
+	}
+}
